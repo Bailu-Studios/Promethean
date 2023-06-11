b3fd6a0406e03f8b086456d70bb99f271fefe80b
Gu_ZT <administration@ztxy666.cn>
2023-06-12 05:09:31 +0800

    优化错误处理

--- src/promethean/api/event/event.py
+++ src/promethean/api/event/event.py
@@ -2,5 +2,14 @@ from abc import ABC
 
 
 class Event(ABC):
+    is_cancelable: False
+    is_canceled: False
+
+    def is_cancelable(self):
+        return self.is_cancelable
+
+    def is_canceled(self):
+        return self.is_cancelable
+
     def __init__(self):
         ...
--- src/promethean/api/event/event_manager.py
+++ src/promethean/api/event/event_manager.py
@@ -1,6 +1,7 @@
+import asyncio
 from typing import List, Dict, Callable, Coroutine, Any
 from typing import Type as Class
-from inspect import signature, Signature
+from inspect import signature, Signature, Parameter
 from .event import Event
 
 EventHandler = Callable[[Event], Coroutine[Any, Any, Any]]
@@ -12,14 +13,39 @@ class EventManager:
     def __init__(self):
         self.map = {}
 
-    async def post(self, context: Event):
+    async def post(self, context: Event, callback: Callable[[], Coroutine[Any, Any, Any]] = None):
         func_list: List[EventHandler] = self.map.get(context.__class__, [])
         for i in func_list:
             await i(context)
+        if context.is_cancelable and not context.is_canceled:
+            await callback()
 
     def subscribe(self, handler: EventHandler):
-        sig: Signature = signature(handler)
-        event_type: Class[Event] = list(sig.parameters.values())[0].annotation
+        sign: Signature = signature(handler)
+        params: List[Parameter] = list(sign.parameters.values())
+        if len(params) != 1:
+            raise UnsupportedFunctionException('Params lens not match.')
+        event_type: Class[Event] = params[0].annotation
         func_list: List[EventHandler] = self.map.get(event_type, [])
-        func_list.append(handler)
+        if not issubclass(event_type, Event):
+            raise IllegalEventException()
+        if not asyncio.iscoroutinefunction(handler):
+            raise UnsupportedFunctionException('Is not coroutine function.')
+        else:
+            func_list.append(handler)
         self.map[event_type] = func_list
+
+
+class UnsupportedFunctionException(Exception):
+    msg: str
+
+    def __init__(self, msg: str):
+        self.msg = msg
+
+    def __str__(self):
+        return self.msg
+
+
+class IllegalEventException(Exception):
+    def __str__(self):
+        return 'Event handle type not match.'
